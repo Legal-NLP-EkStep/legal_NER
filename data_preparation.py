c9dbdf6fd0c06931657d4c9ed24d79087b94eeb1
astha-agarwal99 <astha99agarwal@gmail.com>
2022-08-03 16:52:20 +0530

    Remove hidden text

--- data_preparation.py
+++ data_preparation.py
@@ -164,10 +164,8 @@ def get_text_from_indiankanoon_url(url):
                                                                judgment_txt_tag['id'].startswith('pre_')):
                     for content in judgment_txt_tag.contents:
                         if isinstance(content, Tag):
-                            if content.get('class') is not None and 'hidden_text' in content['class']:
-                                if not check_hidden_text_is_invalid(content.text.strip()):
-                                    tag_txt = tag_txt + content.text
-                            else:
+                            if not(content.get('class') is not None and 'hidden_text' in content['class']):
+
                                 tag_txt = tag_txt + content.text
                         else:
                             tag_txt = tag_txt + str(content)
