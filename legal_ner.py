525897c16433dfaace04a0e8ff5acb0c4b31cb26
astha-agarwal99 <astha99agarwal@gmail.com>
2022-10-03 17:04:49 +0530

    Add:postprocessing on NER

--- legal_ner.py
+++ legal_ner.py
@@ -3,11 +3,12 @@ import spacy
 from spacy import displacy
 import re
 from data_preparation import seperate_and_clean_preamble,get_text_from_indiankanoon_url,get_sentence_docs
+from postprocessing_utils import postprocessing
 from spacy.tokens import Span
 import time
 
 
-def extract_entities_from_judgment_text(txt,legal_nlp,nlp_preamble_splitting,text_type):
+def extract_entities_from_judgment_text(txt,legal_nlp,nlp_preamble_splitting,text_type,do_postprocess):
     ######### Seperate Preamble and judgment text
     seperation_start_time = time.time()
     preamble_text,preamble_end= seperate_and_clean_preamble(txt,nlp_preamble_splitting)
@@ -36,7 +37,8 @@ def extract_entities_from_judgment_text(txt,legal_nlp,nlp_preamble_splitting,tex
     combining_start_time = time.time()
     combined_doc = spacy.tokens.Doc.from_docs([doc_preamble, doc_judgment])
     print("Combining took " + str(time.time() - combining_start_time))
-
+    if do_postprocess:
+        combined_doc=postprocessing(combined_doc)
     return combined_doc
 
 if __name__ == "__main__":
@@ -47,10 +49,11 @@ if __name__ == "__main__":
     legal_nlp = spacy.load('en_legal_ner_trf') ## path of trained model files
     preamble_spiltting_nlp = spacy.load('en_core_web_sm') #### only for splitting the preamble and judgment when keywords are not found
     ########## Extract Entities
-    combined_doc = extract_entities_from_judgment_text(txt,legal_nlp,preamble_spiltting_nlp,text_type='sent')
+    combined_doc = extract_entities_from_judgment_text(txt,legal_nlp,preamble_spiltting_nlp,text_type='sent',do_postprocess=True)
 
     ########### show the entities
     extracted_ent_labels = list(set([i.label_ for i in combined_doc.ents]))
+
     colors = {'COURT': "#bbabf2", 'PETITIONER': "#f570ea", "RESPONDENT": "#cdee81", 'JUDGE': "#fdd8a5",
               "LAWYER": "#f9d380", 'WITNESS': "violet", "STATUTE": "#faea99", "PROVISION": "yellow",
               'CASE_NUMBER': "#fbb1cf", "PRECEDENT": "#fad6d6", 'DATE': "#b1ecf7", 'OTHER_PERSON': "#b0f6a2",
